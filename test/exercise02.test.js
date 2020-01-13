6834be43f5bca4f99ad784e66bf7def1ed64eda0
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 10:48:10 +0000

    exercise 3

--- test/exercise02.test.js
+++ test/exercise02.test.js
@@ -1,20 +1,20 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
 
-suite('Unit Tests Exercise 2 - Definition', function(){
-   // Right now all the tests will automatically fail 
-   // 2 - Use assert.isDefined() or assert.isUndefined() to make the tests pass.
-   test('is null defined?', function(){
-    assert.isDefined( null, 'null is not undefined');
-   });
-   test('is undefined defined?',function(){
-    assert.isUndefined( undefined, 'undefined IS undefined');
-   });
-   test('is hello defined?',function(){
-    assert.isDefined( 'hello', 'a string is not undefined');
-   });
-});
+// suite('Unit Tests Exercise 2 - Definition', function(){
+//    // Right now all the tests will automatically fail 
+//    // 2 - Use assert.isDefined() or assert.isUndefined() to make the tests pass.
+//    test('is null defined?', function(){
+//     assert.isDefined( null, 'null is not undefined');
+//    });
+//    test('is undefined defined?',function(){
+//     assert.isUndefined( undefined, 'undefined IS undefined');
+//    });
+//    test('is hello defined?',function(){
+//     assert.isDefined( 'hello', 'a string is not undefined');
+//    });
+// });
 
  // undefined is the curse of javascript. In JS, you can 
  // DECLARE a variable without DEFINING it, and often the program
