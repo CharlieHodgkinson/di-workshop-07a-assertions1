b3cd318d037a0fe80aa743eeb4ddc9cec4b7a9a2
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 10:56:41 +0000

    exercise 5

--- test/exercise04.test.js
+++ test/exercise04.test.js
@@ -1,21 +1,21 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-suite('Unit Tests Exercise 4 - Truth', function(){
-   // 4 - Use assert.isTrue() or assert.isNotTrue() to make the tests pass.
-   // .isTrue(true) and .isNotTrue(everything else) will work.
-   // .isFalse() and .isNotFalse() also exist.
+// suite('Unit Tests Exercise 4 - Truth', function(){
+//    // 4 - Use assert.isTrue() or assert.isNotTrue() to make the tests pass.
+//    // .isTrue(true) and .isNotTrue(everything else) will work.
+//    // .isFalse() and .isNotFalse() also exist.
 
-   test('is true true?', function(){
-    assert.isNotTrue( null, 'null is not undefined');
-   });
-   test('is a double negation true?',function(){
-    assert.isNotTrue( undefined, 'undefined IS undefined');
-   });
-   test('is a truthy object true?',function(){
-    assert.isNotTrue( 'hello', 'a string is not undefined');
-   });
-});
+//    test('is true true?', function(){
+//     assert.isNotTrue( null, 'null is not undefined');
+//    });
+//    test('is a double negation true?',function(){
+//     assert.isNotTrue( undefined, 'undefined IS undefined');
+//    });
+//    test('is a truthy object true?',function(){
+//     assert.isNotTrue( 'hello', 'a string is not undefined');
+//    });
+// });
 
 // true and false are the cornerstones of boolean, i.e. yes or no
 // logic. there are more assertions like these: .isNaN(), .isBoolean(), 
