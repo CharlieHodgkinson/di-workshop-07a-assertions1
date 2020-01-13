57bed36ac975987d68ecf43199beda2ff94535d2
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 11:21:34 +0000

    exercise 7

--- test/exercise06.test.js
+++ test/exercise06.test.js
@@ -1,20 +1,20 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-suite('Unit Tests Exercise 6 - Strict Equality', function(){
-   // 6. .strictEqual(), .notStrictEqual() 
-   // .strictEqual() compares objects using '==='
-   test('is a number strictly equal to its string version?', function(){
-    assert.notStrictEqual( 6, '6' );
-   });
-   test('is a product strictly equal to its result?',function(){
-    assert.strictEqual( 6, 3*2 );
-   });
-   test('are two arrays strictly equal?',function(){
-    assert.notStrictEqual( [1, 'a', {}], [1, 'a', {}] );
-   });
-});
+// suite('Unit Tests Exercise 6 - Strict Equality', function(){
+//    // 6. .strictEqual(), .notStrictEqual() 
+//    // .strictEqual() compares objects using '==='
+//    test('is a number strictly equal to its string version?', function(){
+//     assert.notStrictEqual( 6, '6' );
+//    });
+//    test('is a product strictly equal to its result?',function(){
+//     assert.strictEqual( 6, 3*2 );
+//    });
+//    test('are two arrays strictly equal?',function(){
+//     assert.notStrictEqual( [1, 'a', {}], [1, 'a', {}] );
+//    });
+// });
 
-// strict equality does not allow for type conversion, and will not
-// evaluate null as being the same as undefined, for example
-// however, the handling of arrays and objects is not different
+// // strict equality does not allow for type conversion, and will not
+// // evaluate null as being the same as undefined, for example
+// // however, the handling of arrays and objects is not different
