9d5d07b52054a8dd61e2394004030a156121cc2b
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 10:41:05 +0000

    exercise 2

--- test/exercise01.test.js
+++ test/exercise01.test.js
@@ -1,20 +1,20 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-suite('Unit Tests Exercise 1 - The Void', function(){    
-    // assert.fail() will always fail. Change it into something more useful... 
-    // 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. 
-    // when you're done, type `npm test` in your terminal to try the tests
-    // if you get an error relating to mocha not being found, type `npm install`
-    // try not to change anything other than the `assert.fail()` in the following code
-    test('Is null null?', function(){
-      // change the below to 'assert.isNull()' and watch the first test pass
-      assert.isNull(null, 'this is an optional error description - e.g. null is null');
-    });
-    test('Is 1 null?', function(){
-      assert.isNotNull( 1, '1 is not null');
-    });
-  });
+// suite('Unit Tests Exercise 1 - The Void', function(){    
+//     // assert.fail() will always fail. Change it into something more useful... 
+//     // 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. 
+//     // when you're done, type `npm test` in your terminal to try the tests
+//     // if you get an error relating to mocha not being found, type `npm install`
+//     // try not to change anything other than the `assert.fail()` in the following code
+//     test('Is null null?', function(){
+//       // change the below to 'assert.isNull()' and watch the first test pass
+//       assert.isNull(null, 'this is an optional error description - e.g. null is null');
+//     });
+//     test('Is 1 null?', function(){
+//       assert.isNotNull( 1, '1 is not null');
+//     });
+//   });
 
     // null is something that you need to explicitly define a variable as being
     // unlike undefined, a variable or property needs to be explicitly defined as null
